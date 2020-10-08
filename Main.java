836622869c1c13baacd0d2e8f3d0adc687b0d290
seoe5580 <seoe5580@wrdsb.ca>
2020-10-08 17:22:38 +0000

    Finished star question

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program will create an x amount of star depending on the user's choice
+ * @author Eric Seo
  */
 public class Main {
 
@@ -9,7 +10,20 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // Creates a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // Asks the user to input a number between 1 ~ 10
+    System.out.println("Please enter a number between 1 and 10");
+
+    // Gets the number from the user
+    int num = input.nextInt();
+
+    // using the for loop it will type "* " until the 1 number before
+    for(int i = 1; i < num; i++){
+      System.out.print("* ");
+    }
+    // This will get rid of the space after the * for the last loop
+    System.out.print("*");
   }
 }
